d44d52d99c59cd381ea8e28338d8825dedd3a2ef
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-02-28 16:16:22 -0600

    init project eith libs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    const { dir } = options;
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: "url-loader",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
